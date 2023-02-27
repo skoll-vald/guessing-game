690b675005156e838c0931e87ecb7b5461ccbc62
skoll-vald <skollvald@gmail.com>
2023-02-27 20:04:44 +0300

    init task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.min_val = null;
+        this.max_val = null;
+        this.guess_val = null;
+    }
+    
     setRange(min, max) {
-
+        this.min_val = min;
+        this.max_val = max;
     }
-
+    
     guess() {
-
+        this.guess_val = Math.floor((this.min_val + this.max_val) / 2);
+        return this.guess_val;
     }
-
+    
     lower() {
-
+        this.max_val = this.guess_val + 1;
     }
-
+    
     greater() {
-
+        this.min_val = this.guess_val;
     }
 }
+
 
 module.exports = GuessingGame;
